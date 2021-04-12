8d8aad0242a112e2b455db5d82ec11df571ef213
jholloway <josh_h1995@hotmail.co.uk>
2021-04-12 20:11:00 +0100

    spring set up

---
+++ src/main/java/com/barclays/tshapeproject/TshapeprojectApplication.java
@@ -0,0 +1,13 @@
+package com.barclays.tshapeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TshapeprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TshapeprojectApplication.class, args);
+	}
+
+}
