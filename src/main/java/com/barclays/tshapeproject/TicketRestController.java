c1dfa0c642527916630c6715acad548b7daa585f
jholloway <josh_h1995@hotmail.co.uk>
2021-04-19 00:13:29 +0100

    the back end works

--- src/main/java/com/barclays/tshapeproject/TicketRestController.java
+++ src/main/java/com/barclays/tshapeproject/TicketRestController.java
@@ -1,38 +1,54 @@
 package com.barclays.tshapeproject;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
 public class TicketRestController {
 
-    private TicketService service;
+    private final TicketService service;
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public TicketRestController(TicketService service) {
         super();
         this.service = service;
     }
     @GetMapping("/getAllTickets")
-    public List<TicketEntity>  getAllTickets(){
-        return this.service.getAllTickets();
+    public ResponseEntity<List<Tickets>> getAllTickets(){
+        LOGGER.info("GetAllTickets request received");
+        return new ResponseEntity<>(this.service.getAllTickets(),HttpStatus.OK);
+
     }
     @GetMapping("/getTicketByID/{id}")
-    public List<TicketEntity>  getTicketById(@PathVariable("id") long id){
-        return this.service.getTicketById(id);
+    public ResponseEntity<Tickets>  getTicketById(@PathVariable("id") long id){
+
+        LOGGER.info("GetTicketById request received with id: " + id );
+        return new ResponseEntity<>(this.service.getTicketById(id),HttpStatus.ACCEPTED);
     }
 
     @GetMapping("/getTicketByStatus/{status}")
-    public List<TicketEntity>  getTicketByStatus(@PathVariable("status") String status){
-        return this.service.getTicketByStatus(status);
+    public ResponseEntity<List<Tickets>>  getTicketByStatus(@PathVariable("status") String status){
+        LOGGER.info("GetTicketByStatus request received with status: " + status );
+        return new ResponseEntity<>(this.service.getTicketsByStatus(status),HttpStatus.OK);
     }
     @PostMapping("/addTicket")
-    public String  addTicket(String author, String title, String status, String description){
-        return (String) this.service.addTicket(author, title, status, description);
+    public ResponseEntity<String>  addTicket(@RequestBody String ticket){
+        LOGGER.info("addTicket request received");
+        this.service.addTicket(ticket);
+        return  ResponseEntity.status(HttpStatus.CREATED).body("Ticket created");
     }
-    @PutMapping("/editTicket/{id}/{field}/{value}")
-    public int  editTicket(@PathVariable("id") long id, @PathVariable("field") String field, @PathVariable("value ")String value){
-        return this.service.editTicket(id, field,value);
+    @PatchMapping("/editTicket/{id}/")
+    public ResponseEntity<String>  editTicket(@PathVariable("id") long id, @PathParam("field") String field,
+                              @PathParam("value")String value){
+        LOGGER.info("editTicket request received with id: " + id + " field: " + field + " value: " + value);
+        this.service.editTicket(id, field,value);
+        return  ResponseEntity.status(HttpStatus.ACCEPTED).body("Ticket edited");
     }
 
 
